13809a1bc0669ab15d4304b9ea582e0b302eea18
Mateusz Gawin <matrix.gaw@gmail.com>
2024-04-30 19:14:53 +0000

    Fixed pi hardware version

--- nixpi/configuration.nix
+++ nixpi/configuration.nix
@@ -1,7 +1,10 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 
-{ inputs, outputs, lib, config, pkgs, ... }: {
+{ inputs, outputs, lib, config, pkgs, ... }:
+let
+  nixosHardwareVersion = "7f1836531b126cfcf584e7d7d71bf8758bb58969";
+in {
   # You can import other NixOS modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/nixos):
